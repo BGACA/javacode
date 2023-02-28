0148af46aff6513fa258e427f7ce3fceda30db21
BGA <2801363177@qq.com>
2023-02-28 11:40:50 +0800

    BGA

--- bga-provider/src/main/java/com/soft/bga/controller/UserClientController.java
+++ bga-provider/src/main/java/com/soft/bga/controller/UserClientController.java
@@ -1,5 +1,6 @@
 package com.soft.bga.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/provider")
 public class UserClientController {
+
+    @Value(value = "${server.port}")
+    int port;
+
     @GetMapping
-    public String test(){
+    public String test() {
         return "hello ,  i am  provider client";
     }
 
